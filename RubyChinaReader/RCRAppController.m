4102787e25cc06e4a7ff2c0962d394b314520f80
James Chen <ashchan@gmail.com>
2012-03-03 16:29:55 +0900

    disabled ARC

--- RubyChinaReader/RCRAppController.m
+++ RubyChinaReader/RCRAppController.m
@@ -21,17 +21,17 @@
 }
 
 - (void)setupToolbar{
-    WebView *webView = [[WebView alloc] initWithFrame:CGRectMake(0, 0, 400, 600)];
+    WebView *webView = [[[WebView alloc] initWithFrame:CGRectMake(0, 0, 400, 600)] autorelease];
     webView.applicationNameForUserAgent = @"Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7";
     webView.mainFrameURL = @"http://www.ruby-china.org/topics";
     [self addView:webView label:@"Topics" image:[NSImage imageNamed:NSImageNameBonjour]];
 
-    NSTextView *textView = [[NSTextView alloc] initWithFrame:NSMakeRect(0, 0, 300, 400)];
+    NSTextView *textView = [[[NSTextView alloc] initWithFrame:NSMakeRect(0, 0, 300, 400)] autorelease];
     [self addView:textView label:@"Account" image:[NSImage imageNamed:NSImageNameUser]];
     [self addView:textView label:@"Options" image:[NSImage imageNamed:NSImageNameAdvanced]];
 
-    NSImageView *view = [[NSImageView alloc] initWithFrame:NSMakeRect(0, 0, 300, 350)];
-    view.image = [[NSImage alloc] initWithContentsOfURL:[NSURL URLWithString:@"http://l.ruby-china.org/photo/74c63894f0c9f138f233889d901f4e31.png"]];
+    NSImageView *view = [[[NSImageView alloc] initWithFrame:NSMakeRect(0, 0, 300, 350)] autorelease];
+    view.image = [[[NSImage alloc] initWithContentsOfURL:[NSURL URLWithString:@"http://l.ruby-china.org/photo/74c63894f0c9f138f233889d901f4e31.png"]] autorelease];
     [self addView:view label:@"About" image:[NSImage imageNamed:NSImageNameInfo]];
 }
 

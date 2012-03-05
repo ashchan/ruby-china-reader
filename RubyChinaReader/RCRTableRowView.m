65177c2a064fa4c20b714a9bdeb8d8b0a1d22d21
James Chen <ashchan@gmail.com>
2012-03-05 22:49:42 +0900

    adjusted row selection

--- RubyChinaReader/RCRTableRowView.m
+++ RubyChinaReader/RCRTableRowView.m
@@ -39,8 +39,8 @@ void DrawSeparatorInRect(NSRect rect) {
 
 - (void)drawSelectionInRect:(NSRect)dirtyRect {
     if (self.selectionHighlightStyle != NSTableViewSelectionHighlightStyleNone) {
-        NSRect selectionRect = NSInsetRect(self.bounds, 5.5, 5.5);
-        [[NSColor colorWithCalibratedWhite:.72 alpha:1.0] setStroke];
+        NSRect selectionRect = NSInsetRect(self.bounds, 2.5, 2.5);
+        [[NSColor colorWithCalibratedWhite:.65 alpha:1.0] setStroke];
         [[NSColor colorWithCalibratedWhite:.82 alpha:1.0] setFill];
         NSBezierPath *selectionPath = [NSBezierPath bezierPathWithRoundedRect:selectionRect xRadius:6 yRadius:6];
         [selectionPath fill];
